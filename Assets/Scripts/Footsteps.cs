f7dcc0cf8d29425a236bc54cb6917f9fc9582c19
WackyDookie <134848096+WackyDookie@users.noreply.github.com>
2026-05-05 19:49:57 +0200

    Update 5.05

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -145,16 +145,12 @@ public class Footsteps : MonoBehaviour
             case "Stone":
             case "Inside_stone":
             case "Outside": // "Outside" również używa parametru "Stone"
-                surfaceParameter = "Stone";
+                surfaceParameter = "stone";
                 break;
             
             case "Wood":
             case "Inside_wood":
-                surfaceParameter = "Wood";
-                break;
-
-            case "Bed":
-                surfaceParameter = "Bed";
+                surfaceParameter = "wood";
                 break;
         }
 
@@ -164,7 +160,7 @@ public class Footsteps : MonoBehaviour
             soundInstance = RuntimeManager.CreateInstance(eventRef);
             soundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject.transform));
             // Ustawia parametr FMOD na podstawie ustalonej wartości.
-            soundInstance.setParameterByNameWithLabel("Footsteps_surface", surfaceParameter); 
+            soundInstance.setParameterByNameWithLabel("footsteps-4", surfaceParameter); 
             soundInstance.start();
             soundInstance.release();
         }
